976e8d7cd6bdd43d197873846f1626c2c7b20fc7
Yassine EL GHALLALI <y.elghallali@gmail.com>
2023-01-11 18:23:57 +0100

    Update exercice1.py

--- exercice1.py
+++ exercice1.py
@@ -1,2 +1,24 @@
+# pip install numpy  # if numpy module not exist in your system
 import numpy as np
 
+def generator(number):
+    return np.random.randint(0, 20, size=number)
+
+def valide(list):
+    valide = []
+    nonValide = []
+    for i in list:
+        if i >= 10:
+            valide.append(i)
+        else:
+            nonValide.append(i)
+    return valide,nonValide
+
+
+
+z = generator(100)
+valide, nonValide= valide(z)
+
+print(z)
+print(valide)
+print(nonValide)
